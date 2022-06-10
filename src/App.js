b011c9b13bd3085d9f62ea6da8c7b9ecbe64a98d
Polina Skrobot <chi.polly.no@gmail.com>
2022-06-09 18:36:46 -0700

    Alert, routing added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,63 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
 import axios from "axios";
 import "./App.css";
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
-  async componentDidMount() {
+  // async componentDidMount() {
+  //   console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
+  //   this.setState({loading:true});
+  //   const resa = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+  //   this.setState({loading:false, users: resa.data});
+  // }
+  searchUsers =async (inputValue) => {
     this.setState({loading:true});
-    const resa = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    this.setState({loading:false, users: resa.data});
+    const resa = await axios.get(`https://api.github.com/search/users?q=${inputValue}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({loading:false, users: resa.data.items});
+  }
+  clearUsers = () => {
+    this.setState({users: [], loading: false})
+  }
+  setAlert = (msg, type) => {
+    this.setState({alert: {msg, type}});
+    setTimeout(()=>{
+      this.setState({alert: null})
+    }, 3000)
   }
   render() {
+    const { users, loading} = this.state
     return (
+      <Router>
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Users loading={this.state.loading} users={this.state.users}></Users>
+          <Alert alert={this.state.alert}/>
+          <Routes>
+            <Route exact path='/' element={
+              <Fragment>
+                <Search searchUsers={this.searchUsers} 
+                        clearUsers={this.clearUsers} 
+                        showClear={users.length? true: false}
+                        setAlert={this.setAlert}></Search>
+                <Users loading={loading} users={users}></Users>
+              </Fragment>
+
+            } />
+            <Route exact path='/about' element={<About></About>} />            
+          </Routes>
         </div>
       </div>
+      </Router>
     );
   }
 }
